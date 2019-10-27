7496eb8406ff222799c40b649b850d0f3f328fad
Raymond Cross <52549515+rcross-42@users.noreply.github.com>
2019-10-27 05:42:04 -0700

    Add files via upload

---
+++ Enemy.Class.hpp
@@ -0,0 +1,24 @@
+#ifndef ENEMY_CLASS_HPP
+# define ENEMY_CLASS_HPP
+
+#include "GameEntity.Class.hpp"
+
+class Enemy : public GameEntity
+{
+private:
+
+public:
+	Enemy();
+	~Enemy();
+};
+
+Enemy::Enemy()
+{
+}
+
+Enemy::~Enemy()
+{
+}
+
+
+#endif
